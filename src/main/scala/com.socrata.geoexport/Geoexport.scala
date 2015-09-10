8d8a711e278b1b39495c43ecbb25453ca55525ce
Chris Duranti <chris.duranti@socrata.com>
2015-09-10 12:20:00 -0700

    make it de-register

--- src/main/scala/com.socrata.geoexport/Geoexport.scala
+++ src/main/scala/com.socrata.geoexport/Geoexport.scala
@@ -17,7 +17,7 @@ import com.socrata.thirdparty.curator._
 import http.{VersionService, ExportService}
 import config.GeoexportConfig
 
-// $COVERAGE-OFF$ Disabled because this is framework boilerplate.
+// $COVERAGE-OFF$ Disabled because ... . .. ...
 object Geoexport extends App {
 
   implicit val shutdownTimeout = Resource.executorShutdownNoTimeout
@@ -37,23 +37,30 @@ object Geoexport extends App {
     upstream <- new UnmanagedDiscoveryBroker(discovery, http).clientFor(GeoexportConfig.upstream)
   } {
 
+    //could wrap this in simple arm stuff but there are more important things in life and this is simple
+    val advertisement = new CuratorBroker(
+      discovery,
+      GeoexportConfig.broker.discovery.address,
+      GeoexportConfig.broker.discovery.name,
+      None
+    )
+    val cookie = advertisement.register(GeoexportConfig.port)
 
-    val wat = new CuratorBroker(discovery, GeoexportConfig.broker.discovery.address, GeoexportConfig.broker.discovery.name, None)
-    wat.register(GeoexportConfig.port)
+    try {
+      val exportService = new ExportService(upstream)
+      val router = new Router(exportService)
 
-    val exportService = new ExportService(upstream)
+      val server = new SocrataServerJetty(
+        handler = router.handler,
+        options = SocrataServerJetty.defaultOptions.
+          withPort(GeoexportConfig.port).
+          withPoolOptions(SocrataServerJetty.Pool(GeoexportConfig.threadpool)))
 
-
-    val router = new Router(exportService)
-
-    val server = new SocrataServerJetty(
-      handler = router.handler,
-      options = SocrataServerJetty.defaultOptions.
-        withPort(GeoexportConfig.port).
-        withPoolOptions(SocrataServerJetty.Pool(GeoexportConfig.threadpool)))
-
-    logger.info("Starting Geoexport")
-    server.run()
+      logger.info("Starting Geoexport")
+      server.run()
+    } finally {
+      advertisement.deregister(cookie)
+    }
   }
 }
   // $COVERAGE-ON$
